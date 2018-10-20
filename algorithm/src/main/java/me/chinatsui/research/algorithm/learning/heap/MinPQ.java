68227f1f8ce0d06c5e0a96afd95038ad991d3b0c
Yaohua <chinatsui1987@gmail.com>
2018-10-20 16:38:56 +0800

    Change MinPQ array index from 1 to 0

--- algorithm/src/main/java/me/chinatsui/research/algorithm/learning/heap/MinPQ.java
+++ algorithm/src/main/java/me/chinatsui/research/algorithm/learning/heap/MinPQ.java
@@ -7,61 +7,62 @@ import java.util.Arrays;
 
 public class MinPQ {
 
-    int[] heap;
-    int count;
+    private final int[] heap;
+    int size;
 
     public MinPQ(int size) {
-        heap = new int[size + 1];
+        heap = new int[size];
+        this.size = 0;
     }
 
-    public void insert(int number) {
-        int index = ++count;
-        heap[index] = number;
-        swim(index);
+    public void offer(int number) {
+        int idx = size++;
+        heap[idx] = number;
+        swim(idx);
     }
 
-    public int deletePriority() {
-        int priority = heap[1];
-        exchange(1, count);
-        heap[count--] = 0;
-        sink(1);
-        return priority;
+    public int poll() {
+        int res = heap[0];
+        swap(0, size - 1);
+        heap[--size] = 0;
+        sink(0);
+        return res;
     }
 
-    public int getCount() {
-        return count;
+    public int getSize() {
+        return size;
     }
 
     private void swim(int index) {
-        int parentIndex = index / 2;
+        int parentIndex = (index - 1) / 2;
         if (heap[index] < heap[parentIndex]) {
-            exchange(parentIndex, index);
+            swap(parentIndex, index);
             swim(parentIndex);
         }
     }
 
-    private void sink(int index) {
-        int leftChildIndex = index * 2;
-        int rightChildIndex = index * 2 + 1;
+    private void sink(int idx) {
+        int leftIdx = (idx + 1) * 2 - 1;
+        int rightIdx = (idx + 1) * 2;
 
-        if (leftChildIndex > count) {
+        if (leftIdx > size - 1) {
             return;
         }
 
-        if (leftChildIndex == count || heap[leftChildIndex] < heap[rightChildIndex]) {
-            if (heap[index] > heap[leftChildIndex]) {
-                exchange(index, leftChildIndex);
-                sink(leftChildIndex);
+        if (leftIdx == size - 1 || heap[leftIdx] < heap[rightIdx]) {
+            if (heap[idx] > heap[leftIdx]) {
+                swap(idx, leftIdx);
+                sink(leftIdx);
             }
         } else {
-            if (heap[index] > heap[rightChildIndex]) {
-                exchange(index, rightChildIndex);
-                sink(rightChildIndex);
+            if (heap[idx] > heap[rightIdx]) {
+                swap(idx, rightIdx);
+                sink(rightIdx);
             }
         }
     }
 
-    private void exchange(int index1, int index2) {
+    private void swap(int index1, int index2) {
         int tmp = heap[index1];
         heap[index1] = heap[index2];
         heap[index2] = tmp;
@@ -69,19 +70,19 @@ public class MinPQ {
 
 
     public static void main(String[] args) {
+        System.out.println(-1 / 2);
         Integer[] nums = DataUtils.getRandomIntegerArray(16);
 
         System.out.println(Arrays.toString(nums));
 
         MinPQ minPQ = new MinPQ(30);
         for (Integer num : nums) {
-            minPQ.insert(num);
+            minPQ.offer(num);
         }
 
-        while (minPQ.getCount() > 0) {
-            System.out.print(minPQ.deletePriority() + " ");
+        while (minPQ.getSize() > 0) {
+            System.out.print(minPQ.poll() + " ");
         }
-
     }
 
 }
